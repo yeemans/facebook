f0b20f999f5c791d8d667e9d94b70e93af34af02
yeemans <arthur.roth.07@gmail.com>
2021-09-02 20:46:46 -0700

    fixed password for sendgrid

--- config/test_set.rb
+++ config/test_set.rb
@@ -1,5 +1,6 @@
 ENV['GOOGLE_CLIENT_ID'] = '364140549557-006pn8dtjcrgvo34m5afddqqjo54clnb.apps.googleusercontent.com'
 ENV['GOOGLE_CLIENT_SECRET'] = 'HsSvOCTBySvUcDkeT-Tt2nFC'
 ENV['PASSWORD'] = 'fbckpfnitsuhhfdl'
-ENV['SENDGRID_USERNAME'] = 'arthur.roth.07@gmail.com'
-ENV['SENDGRID_PASSWORD'] = 'bluea88$Peelord69'
+ENV['SENDGRID_USERNAME'] = 'apikey'
+ENV['SENDGRID_PASSWORD'] = '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
+'
